2bbfacf8e309f1272fc23e661c90637d06cda883
Yaroslav Buzovskyi <yaroslavbuzovsky05@gmail.com>
2026-05-25 16:45:19 +0300

    added route settings and navbar updates

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import cart_icon from '../Assets/cart_icon.png'
+import user_icon from '../Assets/user_icon.png'
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
 
@@ -8,19 +10,21 @@ export const Navbar = () => {
 
     return (
     <div className='navbar'>
+        <div className="container">
         <div className="nav-logo">
             <p>ARKON</p>
         </div>
         <ul className="nav-menu">
-            <li onClick={() => setMenu("home")}>Home{menu === "home"?<hr/>:<></>}</li>
-            <li onClick={() => setMenu("mens")}>Men{menu === "mens"?<hr/>:<></>}</li>
-            <li onClick={() => setMenu("womens")}>Women{menu === "womens"?<hr/>:<></>}</li>
-            <li onClick={() => setMenu("kids")}>Kids{menu === "kids"?<hr/>:<></>}</li>
+            <li onClick={() => setMenu("home")}><Link to='/'>Home</Link>{menu === "home"?<hr/>:<></>}</li>
+            <li onClick={() => setMenu("mens")}><Link to='/mens'>Men</Link>{menu === "mens"?<hr/>:<></>}</li>
+            <li onClick={() => setMenu("womens")}><Link to='/womens'>Womens</Link>{menu === "womens"?<hr/>:<></>}</li>
+            <li onClick={() => setMenu("kids")}><Link to='/kids'>Kids</Link>{menu === "kids"?<hr/>:<></>}</li>
         </ul>
         <div className="nav-login-cart">
-            <img src={cart_icon} alt="" />
+            <Link to='/cart'><img src={cart_icon} alt="" /></Link>
             <div className="nav-cart-count">0</div>
-            <button>Login</button>
+            <Link to='/login'><img src={user_icon} alt="" /></Link>
+        </div>
         </div>
     </div>
     )
