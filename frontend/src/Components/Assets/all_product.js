9fac3c2369456782f74c341422d2a3780ffa1810
Yaroslav Buzovskyi <yaroslavbuzovsky05@gmail.com>
2026-05-25 14:36:26 +0300

    project settings

---
+++ frontend/src/Components/Assets/all_product.js
@@ -0,0 +1,167 @@
+import p1_img from "./product_1.png";
+import p2_img from "./product_2.png";
+import p3_img from "./product_3.png";
+import p4_img from "./product_4.png";
+import p5_img from "./product_5.png";
+import p6_img from "./product_6.png";
+import p7_img from "./product_7.png";
+import p8_img from "./product_8.png";
+import p9_img from "./product_9.png";
+import p10_img from "./product_10.png";
+import p11_img from "./product_11.png";
+import p12_img from "./product_12.png";
+import p13_img from "./product_13.png";
+import p14_img from "./product_14.png";
+import p15_img from "./product_15.png";
+import p16_img from "./product_16.png";
+import p17_img from "./product_17.png";
+import p18_img from "./product_18.png";
+
+let all_product = [
+  {
+    id: 1,
+    name: "Classic wool coat",
+    category: "men",
+    image: p1_img,
+    new_price: 0.0,
+    old_price: 170.0,
+  },
+  {
+    id: 2,
+    name: "Slim-fit jeans",
+    category: "men",
+    image: p2_img,
+    new_price: 0.0,
+    old_price: 55.0,
+  },
+  {
+    id: 3,
+    name: "Oxford shirt",
+    category: "men",
+    image: p3_img,
+    new_price: 0.0,
+    old_price: 30.0,
+  },
+  {
+    id: 4,
+    name: "Merino wool sweater",
+    category: "men",
+    image: p4_img,
+    new_price: 0.0,
+    old_price: 70.0,
+  },
+  {
+    id: 5,
+    name: "Cargo pants",
+    category: "men",
+    image: p5_img,
+    new_price: 0.0,
+    old_price: 65.0,
+  },
+  {
+    id: 6,
+    name: "Leather belt",
+    category: "men",
+    image: p6_img,
+    new_price: 0.0,
+    old_price: 20.0,
+  },
+  {
+    id: 7,
+    name: "Linen blazer",
+    category: "women",
+    image: p7_img,
+    new_price: 0.0,
+    old_price: 88.0,
+  },
+  {
+    id: 8,
+    name: "Silk midi dress",
+    category: "women",
+    image: p8_img,
+    new_price: 0.0,
+    old_price: 103.0,
+  },
+  {
+    id: 9,
+    name: "Silk blouse",
+    category: "women",
+    image: p9_img,
+    new_price: 0.0,
+    old_price: 60.0,
+  },
+  {
+    id: 10,
+    name: "Pleated midi skirt",
+    category: "women",
+    image: p10_img,
+    new_price: 0.0,
+    old_price: 45.0,
+  },
+  {
+    id: 11,
+    name: "Voluminous cardigan",
+    category: "women",
+    image: p11_img,
+    new_price: 0.0,
+    old_price: 65.0,
+  },
+  {
+    id: 12,
+    name: "Leather tote bag",
+    category: "women",
+    image: p12_img,
+    new_price: 0.0,
+    old_price: 125.0,
+  },
+  {
+    id: 13,
+    name: "Pelisse",
+    category: "kid",
+    image: p13_img,
+    new_price: 0.0,
+    old_price: 56.5,
+  },
+  {
+    id: 14,
+    name: "Overalls for babies",
+    category: "kid",
+    image: p14_img,
+    new_price: 0.0,
+    old_price: 33.50,
+  },
+  {
+    id: 15,
+    name: "Duffle",
+    category: "kid",
+    image: p15_img,
+    new_price: 0.0,
+    old_price: 40.0,
+  },
+  {
+    id: 16,
+    name: "School dress",
+    category: "kid",
+    image: p16_img,
+    new_price: 0.0,
+    old_price: 30.0,
+  },
+  {
+    id: 17,
+    name: "Winter jacket",
+    category: "kid",
+    image: p17_img,
+    new_price: 0.0,
+    old_price: 78.0,
+  },
+  {
+    id: 18,
+    name: "Backpack",
+    category: "kid",
+    image: p18_img,
+    new_price: 0.0,
+    old_price: 22.0,
+  },
+];
+
+export default all_product;
