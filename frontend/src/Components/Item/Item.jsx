6cb7e70f91614dcde9d7f9ec3c134a46c2ea2820
Yaroslav Buzovskyi <yaroslavbuzovsky05@gmail.com>
2026-05-26 16:20:18 +0300

    feat: created the trending now section and added a description product to the item.jxs and data.js

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -6,12 +6,13 @@ const Item = (props) => {
         <div className='item'>
             <img src={props.image} alt="" />
             <p>{props.name}</p>
+            <span>{props.description}</span>
             <div className="item-prices">
                 <div className="item-price-new">
-                    {props.new_price}
+                    {props.new_price}$
                 </div>
                 <div className="item-price-old">
-                    {props.old_price}
+                    {props.old_price}$
                 </div>
             </div>
         </div>
